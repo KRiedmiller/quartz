b570a072b898e287167201be5e89563ea56801ed
Kai Riedmiller <kai.riedmiller@web.de>
2024-01-13 13:27:55 +0100

    file dates

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -1,20 +1,51 @@
 import { formatDate, getDate } from "./Date"
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import readingTime from "reading-time"
+import type { JSX } from "preact"
+
 
 export default (() => {
   function ContentMetadata({ cfg, fileData, displayClass }: QuartzComponentProps) {
     const text = fileData.text
     if (text) {
-      const segments: string[] = []
+      // const segments: string[] = []
+      const segments: JSX.Element[] = []
       const { text: timeTaken, words: _words } = readingTime(text)
 
+      // if (fileData.dates) {
+      //   segments.push(formatDate(getDate(cfg, fileData)!))
+      // }
       if (fileData.dates) {
-        segments.push(formatDate(getDate(cfg, fileData)!))
+        if (fileData.dates.created) {
+          segments.push(
+            <span>
+              🌿 Planted {formatDate(fileData.dates.created)}
+            </span>,
+          )
+        }
+
+        if (fileData.dates.modified) {
+          segments.push(
+            <span>
+              🧤 Last tended {formatDate(fileData.dates.modified)}
+            </span>,
+          )
+        }
       }
 
-      segments.push(timeTaken)
-      return <p class={`content-meta ${displayClass ?? ""}`}>{segments.join(", ")}</p>
+      segments.push(<span>⏲ {timeTaken}</span>)
+
+      // return <p class={`content-meta ${displayClass ?? ""}`}>{segments.join(", ")}</p>
+      return (
+        <p class="content-meta">
+          {segments.map((meta, idx) => (
+            <>
+              {meta}
+              {idx < segments.length - 1 ? <br /> : null}
+            </>
+          ))}
+        </p>
+      )
     } else {
       return null
     }
